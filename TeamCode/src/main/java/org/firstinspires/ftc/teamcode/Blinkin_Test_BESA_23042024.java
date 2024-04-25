1b90c1d6922b3665cba4667eeaf4686fa33b8622
Bee <awesomelycooltheory@gmail.com>
2024-04-24 21:47:09 -0500

    added more telemetry to Blinkin Test. fixed a bug. added more comments to explain the code more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blinkin_Test_BESA_23042024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blinkin_Test_BESA_23042024.java
@@ -21,7 +21,7 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
     boolean variableForTouch2 = false;
     // Allows us to limit the decrementing to once per button press, see below for code
     int patternNumber = 0;
-    // Allows us to assign a number to a pattern, thus allowing us to increment and decrement through a list, see below for code
+    // Allows us to assign a number to a pattern, thus allowing us to increment and decrement through a switch/case statement, see below for code
 
     @Override
     public void runOpMode()  {
@@ -52,15 +52,23 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
                 if (!variableForTouch1) {
                     patternNumber++;
                     if (patternNumber > 17) {
-                        patternNumber = 18;
+                        patternNumber = 0;
                     }
                     variableForTouch1 = true;
                 }
             } else {
                 variableForTouch1 = false;
             }
-            // Incrementing pattern number once per button press, wrapping the the bottom when finished
-            // If you hold button down it will not increment more than once
+            /* Incrementing pattern number once per button press, wrapping the the bottom when finished
+             * If you hold button down it will not increment more than once
+             *
+             * When TouchSensor1 is pressed, it checks to see if variableForTouch1 is false, which by default it is
+             * When variableForTouch1 is false, it increments patternNumber by 1, then checks if patternNumber is over 17
+             * When patternNumber is over 17, it sets patternNumber to 0
+             * It then sets variableForTouch1 to true, preventing it from incrementing patternNumber again on the next loop
+             * The else condition is only met if TouchSensor1 is false, only then setting variableForTouch1 back to false
+             * Therefore only allowing it to increment again after variable for touch has be set back to false
+             */
 
             if (digitalTouch2.getState()) {
                 if (!variableForTouch2) {
@@ -73,10 +81,18 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
             } else {
                 variableForTouch2 = false;
             }
-            // Decrementing pattern number once per button press, wrapping to the top when finished
-            // If you hold the button down it will not decrement more than once
+            /* Decrementing pattern number once per button press, wrapping the the bottom when finished
+             * If you hold button down it will not decrement more than once
+             *
+             * When TouchSensor2 is pressed, it checks to see if variableForTouch2 is false, which by default it is
+             * When variableForTouch2 is false, it decrements patternNumber by 1, then checks if patternNumber is under 0
+             * When patternNumber is under 0, it sets patternNumber to 17
+             * It then sets variableForTouch2 to true, preventing it from decrementing patternNumber again on the next loop
+             * The else condition is only met if TouchSensor2 is false, only then setting variableForTouch2 back to false
+             * Therefore only allowing it to decrement again after variable for touch has be set back to false
+             */
 
-            // There are 17 cases, you can expand or hide the code to the right of the line number
+            // There are 17 cases in this switch/case statement, you can expand or hide the code to the right of the line number
             switch (patternNumber) {
                 case (0):
                     pattern = RevBlinkinLedDriver.BlinkinPattern.HOT_PINK;
@@ -145,6 +161,7 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
             telemetry.addData("Is TouchSensor2 pressed?: ", digitalTouch2.getState());
             telemetry.addData("variableForTouch2 State: ", variableForTouch2);
             telemetry.update();
+            // should add telemetry to driver hub, not sure if it will work
         }
     }
 }
