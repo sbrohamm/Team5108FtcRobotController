e851899c4ef92ed8d0c6337c27b79625a6c02f0e
Bee <awesomelycooltheory@gmail.com>
2024-04-27 20:41:13 -0500

    changed DigitalChannel declaration to TouchSensor, should fix bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blinkin_Test_BESA_23042024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blinkin_Test_BESA_23042024.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp(name = "Blinkin Test", group = "Linear OPMode")
 public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
@@ -12,9 +12,9 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
     // LED strip
     private RevBlinkinLedDriver.BlinkinPattern pattern;
     // Pattern
-    private DigitalChannel digitalTouch1;
+    private TouchSensor digitalTouch1;
     // Touch sensor 1
-    private DigitalChannel digitalTouch2;
+    private TouchSensor digitalTouch2;
     // Touch sensor 2
     private boolean variableForTouch1 = false;
     // Allows us to limit the incrementing to once per button press, see below for code
@@ -31,13 +31,11 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
         ledStrip.setPattern(pattern);
         // Communicating with the LedStrip and setting idle to STROBE_GOLD
 
-        digitalTouch1 = hardwareMap.get(DigitalChannel.class, "digitalTouch1");
-        digitalTouch1.setMode(DigitalChannel.Mode.INPUT);
-        // Communicating with TouchSensor1 and setting the mode to input
+        digitalTouch1 = hardwareMap.get(TouchSensor.class, "digitalTouch1");
+        // Communicating with TouchSensor1
 
-        digitalTouch2 = hardwareMap.get(DigitalChannel.class, "digitalTouch2");
-        digitalTouch2.setMode(DigitalChannel.Mode.INPUT);
-        // Communicating with TouchSensor2 and setting mode to input
+        digitalTouch2 = hardwareMap.get(TouchSensor.class, "digitalTouch2");
+        // Communicating with TouchSensor2
 
         telemetry.addData("Blinkin LED controller Test Program", "Press Start to Initiate Test.");
         telemetry.update();
@@ -48,7 +46,7 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
         while (opModeIsActive()) {
         // When play is pressed, run the code inside this loop
 
-            if (!digitalTouch1.getState()) {
+            if (digitalTouch1.isPressed()) {
                 if (!variableForTouch1) {
                     patternNumber++;
                     if (patternNumber > 17) {
@@ -62,9 +60,6 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
             /* Incrementing pattern number once per button press, wrapping the the bottom when finished
              * If you hold button down it will not increment more than once
              *
-             * digitalTouch1 is inverted because of the way the rev touch sensor functions.
-             * When the touch sensor is unpressed, it returns true, and when it is pressed it returns false.
-             *
              * When TouchSensor1 is pressed, it checks to see if variableForTouch1 is false, which by default it is
              * When variableForTouch1 is false, it increments patternNumber by 1, then checks if patternNumber is over 17
              * When patternNumber is over 17, it sets patternNumber to 0
@@ -73,7 +68,7 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
              * Therefore only allowing it to increment again after variable for touch has be set back to false
              */
 
-            if (!digitalTouch2.getState()) {
+            if (digitalTouch2.isPressed()) {
                 if (!variableForTouch2) {
                     patternNumber--;
                     if (patternNumber < 0) {
@@ -86,9 +81,6 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
             }
             /* Decrementing pattern number once per button press, wrapping the the bottom when finished
              * If you hold button down it will not decrement more than once
-             *
-             * digitalTouch1 is inverted because of the way the rev touch sensor functions.
-             * When the touch sensor is unpressed, it returns true, and when it is pressed it returns false.
              *
              * When TouchSensor2 is pressed, it checks to see if variableForTouch2 is false, which by default it is
              * When variableForTouch2 is false, it decrements patternNumber by 1, then checks if patternNumber is under 0
@@ -162,9 +154,9 @@ public class Blinkin_Test_BESA_23042024 extends LinearOpMode {
 
             telemetry.addData("Pattern Name: ", pattern.toString());
             telemetry.addData("Pattern Number: ", patternNumber);
-            telemetry.addData("Is TouchSensor1 Pressed?: ", digitalTouch1.getState());
+            telemetry.addData("Is TouchSensor1 Pressed?: ", digitalTouch1.isPressed());
             telemetry.addData("variableForTouch1 State: ", variableForTouch1);
-            telemetry.addData("Is TouchSensor2 pressed?: ", digitalTouch2.getState());
+            telemetry.addData("Is TouchSensor2 pressed?: ", digitalTouch2.isPressed());
             telemetry.addData("variableForTouch2 State: ", variableForTouch2);
             telemetry.update();
             // should add telemetry to driver hub, not sure if it will work
